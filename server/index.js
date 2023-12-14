1b39b0bb95fde51a4640c8f59ab2948413ba3a1a
karimReemo” <karim@reemo.nl>
2023-12-14 19:09:20 +0200

    Fixed issue with servee #KT

--- server/index.js
+++ server/index.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const db = require('./queries')
+const express = require('express');
+const bodyParser = require('body-parser');
 const { exec } = require('child_process');
-const app = express()
-const port = 4000
 
-app.use(bodyParser.json())
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.get('/', response.status(200).json({message:"Hello from QF BE"}))
-app.get('/run', exec('python3 ~/SharqScan/SharqScan.py 8 fantasy.premierleague.com', (error, stdout, stderr) => {
-  if (error) {
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from QF BE' });
+});
 
-    return response.status(200).json({message:`Error executing command: ${error.message}`})
-  }
+app.get('/run', (req, res) => {
+  exec('python3 ~/SharqScan/SharqScan.py 8 fantasy.premierleague.com', (error, stdout, stderr) => {
+    if (error) {
+      res.status(500).json({ message: `Error executing command: ${error.message}` });
+      return;
+    }
 
-  return response.status(200).json({message:`stdout: ${stdout}/ stderr: ${stderr}`})
+    res.status(200).json({ message: `stdout: ${stdout}/ stderr: ${stderr}` });
+  });
+});
 
-
-}))
-
-  
-  app.listen(port, () => {
-    console.log(`Hello world! App running on port ${port}.`)
-  })
+app.listen(port, () => {
+  console.log(`Hello world! App running on port ${port}.`);
+});
