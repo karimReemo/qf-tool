9d162bfe500d58a04a504681c897299be177b124
karimReemo” <karim@reemo.nl>
2023-12-14 19:32:06 +0200

    Added exec code to excute the python command #KT

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { spawnSync } = require('child_process');
+const { exec } = require('child_process');
 
-const { readFile } = require('fs/promises');
-const { appendFile } = require('fs/promises');
-const { join } = require('path');
 const app = express();
 const port = 4000;
 
@@ -21,36 +18,15 @@ app.get('/', (req, res) => {
 
 app.get('/run', (req, res) => {
   try{
-
-    const command = 'python3';
-    const args = [
-      '~/SharqScan/SharqScan.py',
-      '10',
-      'fantasy.premierleague.com',
-    ];
-    
-    const options = {
-      encoding: 'utf-8',
-      stdio: 'inherit', // Use 'inherit' to print output to the console
-    };
-    
-    const pythonProcess = spawnSync(command, args, options);
-    
-    // Check the exit code to determine if the command was successful
-    if (pythonProcess.status === 0) {
-      res.status(200).json({ message: `Command executed successfully` });
-
-      console.log('Sucesss');
-    } else {
-      console.error('Command failed');
-      res.status(500).json({ message: `Error: ${pythonProcess.stderr}` });
-
-      console.error('Error:',pythonProcess.stderr );
-    }
-    
-    // Access the command output
-    console.log('Command output:', pythonProcess.stdout);
-    
+    exec('python3 ~/SharqScan/SharqScan.py 31 fantasy.premierleague.com', (error, stdout, stderr) => {
+      if (error) {
+        console.log('error')
+        res.status(500).json({ message: `Error executing command: ${error.message}` });
+        return;
+      }
+  
+      res.status(200).json({ message: `stdout: ${stdout}/ stderr: ${stderr}` });
+    });
   }
   catch(err){
     res.status(500).json({ message: `Catched error: ${err}` });
@@ -59,13 +35,6 @@ app.get('/run', (req, res) => {
  
 });
 
-
-
-
-
-
 app.listen(port, () => {
   console.log(`Hello world! App running on port ${port}.`);
 });
-
-
