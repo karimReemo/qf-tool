9668b2d714e30a7793d5e085d224125459132b51
karimReemo” <karim@reemo.nl>
2023-12-14 19:07:16 +0200

    Added exec code to excute the python command #KT

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./queries')
+const { exec } = require('child_process');
 const app = express()
 const port = 4000
 
@@ -11,7 +12,18 @@ app.use(
   })
 )
 
-app.get('/', db.getMockData)
+app.get('/', response.status(200).json({message:"Hello from QF BE"}))
+app.get('/run', exec('python3 ~/SharqScan/SharqScan.py 8 fantasy.premierleague.com', (error, stdout, stderr) => {
+  if (error) {
+
+    return response.status(200).json({message:`Error executing command: ${error.message}`})
+  }
+
+  return response.status(200).json({message:`stdout: ${stdout}/ stderr: ${stderr}`})
+
+
+}))
+
   
   app.listen(port, () => {
     console.log(`Hello world! App running on port ${port}.`)
