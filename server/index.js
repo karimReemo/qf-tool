ab55ab2db9693e7450720a624135bcb05b023a08
karimReemo” <karim@reemo.nl>
2023-12-26 15:38:43 +0200

    Made some changes #KT

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidv4, validate: validateUuid } = require("uuid");
+const { exec } = require("child_process");
+const pgp = require("pg-promise")();
 
 const app = express();
 const port = 4000;
+
+const db = pgp({
+  user: "vallumScan",
+  password: "c]QaTm736>+S{unkq9D}L[",
+  host: "vallum-scan.postgres.database.azure.com",
+  port: 5432,
+  database: "vallumscan",
+  ssl: true,
+});
 
 app.use(bodyParser.json());
 app.use(
@@ -12,27 +23,71 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from QF BE' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from QF BE" });
 });
 
-app.get('/run', (req, res) => {
-  try{
-    exec('python3 ~/SharqScan/SharqScan.py 80  sharqsec.com', (error, stdout, stderr) => {
-      if (error) {
-        console.log('error')
-        res.status(500).json({ message: `Error executing command: ${error.message}` });
-        return;
-      }
-  
-      res.status(200).json({ message: `stdout: ${stdout}/ stderr: ${stderr}` });
-    });
-  }
-  catch(err){
-    res.status(500).json({ message: `Catched error: ${err}` });
+app.get("/run-test", (req, res) => {
+  try {
+    // Access query parameters
+    const uuid = req.query.uuid;
+    const website = req.query.website;
 
+    // Validate UUID
+    if (!uuid || !validateUuid(uuid)) {
+      // If the UUID is missing or invalid, return an error response
+      return res.status(400).json({ error: "Invalid or missing UUID v4" });
+    }
+
+    if (!website) {
+      // If the Website is missing or invalid, return an error response
+      return res.status(400).json({ error: "Missing website" });
+    }
+    // Extract domain name and TLD from the URL
+    const url = new URL(website);
+    const domain = url.hostname;
+    const tld = url.pathname.split(".").pop(); // Extract TLD from pathname
+    // Concatenate domain and TLD
+    const fullDomain = `${domain}.${tld}`;
+
+    exec(
+      `python3 ~/SharqScan/SharqScan.py ${uuid}  ${fullDomain}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log("error");
+          res
+            .status(500)
+            .json({ message: `Error executing command: ${error.message}` });
+          return;
+        }
+
+        res
+          .status(200)
+          .json({ message: `Successfully ran test for ${fullDomain}` });
+      }
+    );
+  } catch (err) {
+    res.status(500).json({ message: `Catched error: ${err}` });
   }
- 
+});
+
+app.get("/db/:uuid", async (req, res) => {
+  const uuid = req.params.uuid;
+  try {
+    const record = await db.oneOrNone(
+      "SELECT * FROM wapiti WHERE uuid = $1",
+      uuid
+    );
+    if (record) {
+      res.status(200).json({ record });
+    } else {
+      res.status(404).json({ message: "Record not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Error fetching record: ${error.message}` });
+  }
 });
 
 app.listen(port, () => {
