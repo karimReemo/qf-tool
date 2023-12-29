aa7f72e30f0b550151ae83943c199466acc77ed6
karimReemo” <karim@reemo.nl>
2023-12-29 14:45:15 +0200

    Made some FEchanges to communicate with the BE and allowed cors on the BE #KT

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const db = pgp({
 });
 
 app.use(bodyParser.json());
-app.use(cors({origin: 'http://localhost:5173/'}));
+app.use(cors({origin: 'http://localhost:5173'}));
 
 app.use(
   bodyParser.urlencoded({
